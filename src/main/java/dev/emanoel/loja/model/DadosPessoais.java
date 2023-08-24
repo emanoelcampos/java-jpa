b90c38d2a98e2b09908540aec640572b17b5263c
emanoelcampos <emanoel.cv@gmail.com>
2023-08-24 17:13:54 -0300

    entidades com embebbable

---
+++ src/main/java/dev/emanoel/loja/model/DadosPessoais.java
@@ -0,0 +1,27 @@
+package dev.emanoel.loja.model;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class DadosPessoais {
+
+    private String nome;
+    private String cpf;
+
+    public DadosPessoais(String nome, String cpf) {
+        this.nome = nome;
+        this.cpf = cpf;
+    }
+
+    public DadosPessoais() {
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+}
